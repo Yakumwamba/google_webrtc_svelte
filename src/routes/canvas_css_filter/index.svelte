11cadbd32ddfc5e8a7e977c5bf7c348fa56ed5da
Yakumwamba <brianlemba2@gmail.com>
2021-12-25 06:41:32 +0200

    feature: Filters added

---
+++ src/routes/canvas_css_filter/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { onMount } from "svelte";
+
+    let canvas;
+    let video;
+    let navigator;
+
+    onMount(() => {
+        console.log(canvas);
+        canvas.width = video.width;
+        canvas.height = video.height;
+        navigator = navigator || window.navigator;
+        window = window;
+    });
+</script>
+
+
+<div> 
+
+
+    
+</div>
