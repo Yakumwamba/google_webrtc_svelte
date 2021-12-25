de403e3c8b58e0939d4b7ed0a09ebdc6c9653881
Yakumwamba <brianlemba2@gmail.com>
2021-12-25 05:54:15 +0200

    canavas

--- src/routes/canvas/index.svelte
+++ src/routes/canvas/index.svelte
@@ -1,33 +1,71 @@
-
-
-
 <script>
-import { onMount } from "svelte";
-
+    import { onMount } from "svelte";
 
     let canvas;
-    let ctx;
+    let video;
+    let navigator;
 
     onMount(() => {
-        canvas = document.getElementById("canvas");
-
-       
+        console.log(canvas);
+        canvas.width = video.width;
+        canvas.height = video.height;
+        navigator = navigator || window.navigator;
+        window = window;
     });
 
+    const enableCameraAndAudio = async () => {
+        console.log(navigator);
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: false,
+            });
 
-   const capture_image = () =>{
+            handleSuccess(stream);
+        } catch (err) {
+            handleError(err);
+        }
 
-    }
+        //video.play();
+    };
+    const capture_image = () => {
+        console.log("capturing images ....", video);
+        canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+    };
+
+    const handleSuccess = (stream) => {
+        video.srcObject = stream;
+        video.play();
+    };
+
+    const handleError = (error) => {
+        console.log("navigator.getUserMedia error: ", error);
+    };
+
+    const stopStream    = () => {
+        console.log("stopping stream ....");
+        const tracks = video.srcObject.getTracks();
+        tracks.forEach((track) => {
+            track.stop();
+            console.log("stopped track ....", track.id);
+        });
+
+        video.srcObject = null;
+    };
 </script>
 
-<div class="flex flex-col content"> 
-<canvas height="480" width="360" bind:this={canvas}>
+<div class="flex flex-col">
+    <video bind:this={video} playsinline width="480" height="360" autoplay />
 
-   
+    <canvas bind:this={canvas} />
 
-</canvas>
-<button class="btn btn-primary">
-    Button
-  </button>
+    <button class="btn btn-secondary" on:click={capture_image}
+        >Take snapshot</button
+    >
+    <button class="btn btn-accent" on:click={enableCameraAndAudio}
+        >Enable Camera</button
+    >
+    <button class="btn btn-warning" on:click={stopStream}
+        >Stop Stream</button
+    >
 </div>
-
