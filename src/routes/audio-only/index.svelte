45c790fc3c11de8718a6ef15c2ee93fa50cb3394
Yakumwamba <brianlemba2@gmail.com>
2021-12-26 06:26:44 +0200

    :recycle: Refactoring code

--- src/routes/audio-only/index.svelte
+++ src/routes/audio-only/index.svelte
@@ -1,13 +1,86 @@
+<script>
+  import { browser } from "$app/env";
 
+  import { onMount } from "svelte";
 
+  let constraints;
+  let windowObject;
+  let navigator;
+  let audio;
+  let errorMessageBox;
+  let audioTracks
+  let audioTrackLabel
 
-<div class="card text-center shadow-2xl">
+  onMount(async () => {
+    console.log(window);
+    windowObject = window;
+    constraints = windowObject.constraints = {
+      audio: true,
+      video: false
+    };
+  
+  });
+
+  const handleSuccess = (stream) => {
+     audioTracks = stream.getAudioTracks();
+    console.log("Got stream with constraints:", constraints);
+    console.log(`Using audio device: ${audioTracks[0].label}`);
+    audioTrackLabel = audioTracks[0].label;
+
+    stream.oninactive = () => {
+      console.log("Stream is inactive");
+    };
+
+    windowObject.stream = stream;
+    console.log("Stream stats", stream);
+    audio.srcObject = stream;
+  };
+
+  const handleError = (error) => {
+    const errorMessage =
+      "navigator.MediaDevices.getUserMedia error: " + error.message;
+    errorMessageBox.innerText = errorMessage;
+    console.error(errorMessage);
+  };
+
+  const startAudioCapture = () => {
  
-    <div class="card-body">
-      <h2 class="card-title"> getUserMedia, audio only</h2> 
-   
+
+    windowObject.navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then(handleSuccess)
+      .catch(handleError);
+  };
+
+  const stopAudioCapture = () => {
+    const stream = windowObject.stream;
+    const tracks = stream.getTracks();
+    tracks.forEach((track) => {
+      track.stop();
+    });
+  };
+</script>
+<div class="flex items-center justify-center w-screen">
+  <div class="card text-center shadow-2xl w-5/12 bg-green-300">
+    <div class="card-body flex items-center justify-center gap-2">
+      <h2 class="card-title text-gray">getUserMedia : audio only</h2>
+  
       <div class="justify-center card-actions">
-        <button class="btn btn-primary">Record  </button>
+        <audio bind:this={audio} controls autoplay />
+        <button
+          class="btn bg-red-400 border-4  rounded-full" 
+          on:click={() => {
+            startAudioCapture();
+          }}>Start Capture</button>
+        <p class="text-red text-sm " bind:this={errorMessageBox} />
+      </div>
+      <div class="w-96 h-32 bg-gray-200 flex flex-start flex-col items-start p-2">
+        <p>Got stream with constraints: <span><strong>{JSON.stringify(constraints)}</strong></span></p>
+        <p>Using audio device : {audioTrackLabel ? audioTrackLabel : "No device connected yet" }</p>
       </div>
     </div>
-</div> 
+  </div>
+
+  
+  
+</div>
