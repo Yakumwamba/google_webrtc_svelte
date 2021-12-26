399c2a466c17c30b7defbd4e4a2f027352cf29bc
Yakumwamba <brianlemba2@gmail.com>
2021-12-26 05:24:11 +0200

    :lipstick: updating UI's

--- src/routes/audio-only/index.svelte
+++ src/routes/audio-only/index.svelte
@@ -2,9 +2,7 @@
 
 
 <div class="card text-center shadow-2xl">
-    <figure class="px-10 pt-10">
-      <img src="https://picsum.photos/id/1005/400/250" class="rounded-xl">
-    </figure> 
+ 
     <div class="card-body">
       <h2 class="card-title"> getUserMedia, audio only</h2> 
    
