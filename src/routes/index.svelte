d5d213bc74244a00c8ec3f2a84d4c6e986254b86
Yakumwamba <brianlemba2@gmail.com>
2022-01-22 19:24:29 +0200

    :lipstick: MORE CHANGES

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,14 +10,21 @@
 	import { goto } from "$app/navigation";
 import { onMount } from "svelte";
 
-	import BIRDS from 'vanta/dist/vanta.birds.min';
+	import NET from 'vanta/dist/vanta.net.min';
 	let backgroundVanta;
 
 
 	onMount( () => {
-		this.backgroundVanta = BIRDS({
-      el: document.getElementById('background'),
-    })
+		this.backgroundVanta = NET({
+  el: "#background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
 	
 	})
 
@@ -36,10 +43,10 @@ import { onMount } from "svelte";
 	<div   class="text-center hero-content absolute  flex justify-center  z-10">
 	  <div class="max-w-md">
 		  <img src="logo.png" alt="svelte_webrtc"  />
-		<h1 class="mb-5 text-5xl font-bold">
+		<h1 class="mb-5 text-5xl font-bold text-white">
 			 Hello, Devs
 			</h1> 
-		<p class="text-xl mb-5">
+		<p class="text-xl mb-5 text-white">
 			  Learn more about webrtc and how it can be used to build a realtime application using svelte and Svelte Kit.
 			</p> 
 			<a href="/webrtc" class="bg-gray-200 hover:bg-gray-100 border-b-4 border-yellow-400 hover:border-gray-200 text-black text-center py-2 px-4 rounded">
